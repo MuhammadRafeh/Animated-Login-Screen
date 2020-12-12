27b301229042ec3c8375e50305ebaec5b00e0d00
MuhammadRafeh <muhammadrafehatique@gmail.com>
2020-12-12 21:00:39 +0300

    Design Login Screen

--- App.js
+++ App.js
@@ -2,14 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+import MusicApp from './app/index'
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
+
+class App extends React.Component {
+  state = {
+    isReady: false
+  }
+
+  async _loadAssetsAsync() {
+   const imageAssets = cacheImages([
+     require('./assets/bg.jpg')
+   ]);
+
+   await Promise.all([...imageAssets]);
+ }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return <MusicApp />
+  }
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
